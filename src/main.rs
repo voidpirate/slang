034ae8bb3c259eaa5e3d3468bb5b2a5ba28af902
Brandon Davis <dbrandon.91@gmail.com>
2020-01-20 21:05:20 -0800

    Add simple REPL for testing token generation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use slang::lexer::Lexer;
+use slang::token::TokenType;
+use std::io::{stdin, stdout, Write};
+
+const PROMPT: &str = ">> ";
+const VERSION: &str = "0.0.1";
+
 fn main() {
-    println!("Hello world!")
+    println!("Slang {}", VERSION);
+
+    loop {
+        let mut input = String::new();
+        print_prompt();
+        match stdin().read_line(&mut input) {
+            Ok(_) => match Lexer::new(Some(&input)) {
+                Ok(lex) => print_tokens_from_line(lex),
+                Err(error) => println!("{}", error),
+            },
+            Err(error) => println!("error: {}", error),
+        }
+    }
+}
+
+fn print_tokens_from_line(lex: Lexer) {
+    for token in lex {
+        if token == TokenType::EOF() {
+            break;
+        }
+        println!("Token: {:?}", token)
+    }
+}
+
+fn print_prompt() {
+    print!("{}", PROMPT);
+    let flush_res = stdout().flush();
+    if !flush_res.is_ok() {
+        println!("Unable to flush stdout buffer")
+    }
 }
